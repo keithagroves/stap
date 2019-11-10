1148ae4987ff1568a37f3978fe7094e2ec480531
keithagroves <keith.groves@jointheleague.org>
2019-11-10 08:46:46 -0800

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -2,7 +2,7 @@ echo What would you like to install?
 read varname
 echo installing $varname
 
-if [[ "$varname" = "Keith" ]];
+if [[ "$varname" == "Keith" ]];
 then
   bash -c "`curl -L https://raw.githubusercontent.com/keithagroves/stap/master/install.sh`"
   pwd
