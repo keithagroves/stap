ddb930c3a33ad233c770c77462faa874ecc55223
keithagroves <keith.groves@jointheleague.org>
2019-11-10 08:55:47 -0800

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -4,7 +4,7 @@ echo installing $varname
 
 if [[ "$varname" == "Keith" ]];
 then
-  bash -c "`curl -L https://raw.githubusercontent.com/keithagroves/stap/master/install.sh`"
+  bash -c "`curl -L https://raw.githubusercontent.com/keithagroves/stap/master/install.sh?$(date +%s)`"
   pwd
 fi
 
