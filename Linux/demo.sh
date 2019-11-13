67f647be358146dd0b90aec4cefb10086ecc22f6
keithagroves <keith.groves@jointheleague.org>
2019-11-13 08:49:27 -0800

    loading %

--- Linux/demo.sh
+++ Linux/demo.sh
@@ -2,10 +2,11 @@ echo "installing a terminal program for Linux"
 for i in {0..5}
 do
   echo "Downloading: $i"
-  for i in {0..5}
-    do
-        echo -ne ".%\033[0K\r"
+    for pc in $(seq 1 100); do
+     echo -ne "$pc%\033[0K\r"
+      sleep 1
     done
+    echo
   sleep 2s
 done
 echo "install complete"
